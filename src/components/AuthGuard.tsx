63b57f92298a60935c6664c84d3c648f62139607
Arafian Hafiz <arafianw@gmail.com>
2025-05-17 09:49:19 +0700

    p

--- src/components/AuthGuard.tsx
+++ src/components/AuthGuard.tsx
@@ -2,13 +2,14 @@
 
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, PropsWithChildren } from "react";
+import Cookies from "js-cookie";
 
 export function AuthGuard({ children }: PropsWithChildren) {
   const pathname = usePathname();
   const router = useRouter();
 
   useEffect(() => {
-    const accessToken = localStorage.getItem("accessToken");
+    const accessToken = Cookies.get("accessToken");
 
     if (!accessToken && !["/auth/sign-in", "/auth/sign-up"].includes(pathname)) {
       router.push("/auth/sign-in");
