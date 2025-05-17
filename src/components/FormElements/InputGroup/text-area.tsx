1cee26693ab4df8bbe12646aba7eaf2cb129cb45
Arafian Hafiz <arafianw@gmail.com>
2025-05-18 03:39:32 +0700

    Update text-area.tsx

--- src/components/FormElements/InputGroup/text-area.tsx
+++ src/components/FormElements/InputGroup/text-area.tsx
@@ -10,6 +10,7 @@ interface PropsType {
   className?: string;
   icon?: React.ReactNode;
   defaultValue?: string;
+  name?: string;
 }
 
 export function TextAreaGroup({
@@ -21,6 +22,7 @@ export function TextAreaGroup({
   className,
   icon,
   defaultValue,
+  name,
 }: PropsType) {
   const id = useId();
 
@@ -39,6 +41,7 @@ export function TextAreaGroup({
           rows={6}
           placeholder={placeholder}
           defaultValue={defaultValue}
+          name={name}
           className={cn(
             "w-full rounded-lg border-[1.5px] border-stroke bg-transparent px-5.5 py-3 text-dark outline-none transition focus:border-primary disabled:cursor-default disabled:bg-gray-2 data-[active=true]:border-primary dark:border-dark-3 dark:bg-dark-2 dark:text-white dark:focus:border-primary dark:disabled:bg-dark dark:data-[active=true]:border-primary",
             icon && "py-5 pl-13 pr-5",
